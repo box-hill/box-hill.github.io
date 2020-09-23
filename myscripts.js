b997e803e193b827a96088577702ce5f6516a0e8
box-hill <71170497+box-hill@users.noreply.github.com>
2020-09-23 15:22:00 +1000

    google api not working

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,18 @@
+
 function errorCode(){
 // add error codes and switch case
 	alert('Error 1');
 }
 
-function  getData(){
-	const api_url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY&cx=017576662512468239146:omuauf_lfve&q=hello&callback=hndlr'
+async function getData(){
+	const api_url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY&cx=017576662512468239146:omuauf_lfve&q=lectures';
 
-	const response = await fetch(url);
-  const data = await response.json();
-	console.log(data.notsure);
 
-// let results = {
-//
-//};
-// fetch(url)
-// url
-// take input from user
+	const response = await fetch(api_url);
+  //var data = await response.json();
+	var data = await JSON.parse(reponse);
+	//console.log(response);
+
+	console.log(data.kind);
+	document.getElementsById('myData').testContent = data.hndlr;
 }
