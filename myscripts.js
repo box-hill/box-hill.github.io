ff026c01643176f4923c1b1bf4cb481347710d03
box-hill <71170497+box-hill@users.noreply.github.com>
2020-09-23 15:53:43 +1000

    working searches

--- myscripts.js
+++ myscripts.js
@@ -1,18 +1,23 @@
+// myscripts.js
+
+// example :
+// https://stackoverflow.com/questions/11274056/using-google-search-api-from-the-browser
+
+// google custom search api key:
+// AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY
+
+
 
 function errorCode(){
 // add error codes and switch case
 	alert('Error 1');
 }
 
-async function getData(){
-	const api_url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY&cx=017576662512468239146:omuauf_lfve&q=lectures';
-
-
-	const response = await fetch(api_url);
-  //var data = await response.json();
-	var data = await JSON.parse(reponse);
-	//console.log(response);
-
-	console.log(data.kind);
-	document.getElementsById('myData').testContent = data.hndlr;
+function getData(response) {
+	//src = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY&cx=017576662512468239146:omuauf_lfve&q=cars&callback=hndlr";
+	for (var i = 0; i < response.items.length; i++) {
+		var item = response.items[i];
+		// in production code, item.htmlTitle should have the HTML entities escaped.
+		document.getElementById("test").innerHTML += "<br>" + item.htmlTitle;
+	}
 }
