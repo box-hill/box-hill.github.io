299922ce7ba50461c7584ff17b9076a7e6a108c2
box-hill <71170497+box-hill@users.noreply.github.com>
2020-09-23 20:57:27 +1000

    trigger search on button working

---
+++ cs.js
@@ -0,0 +1,18 @@
+
+var key = "AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY";
+var id = "017576662512468239146:omuauf_lfve"; // Custom Search Engine ID should correspond to quotation searches, currently just a random one
+var q = "cars"; // query input
+
+
+function hndlr(response) {
+  console.log(response);                 // a way to see your results
+}
+
+function triggersearch(){
+  var query=document.getElementById("query").value;
+  var JSElement = document.createElement('script');
+  JSElement.src = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${id}&q=${q}`+query+'&callback=hndlr';
+  document.getElementsByTagName('head')[0].appendChild(JSElement);
+}
+
+triggersearch();
--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,24 @@
 // myscripts.js
 
+// google custom search api documentation:
+// https://developers.google.com/custom-search/v1/overview
+
 // example :
 // https://stackoverflow.com/questions/11274056/using-google-search-api-from-the-browser
 
-// google custom search api key:
-// AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY
+// triggerring search not on load
+// https://stackoverflow.com/questions/16040889/google-custom-search-api-in-a-js-file
 
+// google custom search api key:
+var api_key = "AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY";
+var cse_id = "017576662512468239146:omuauf_lfve"; // Custom Search Engine ID should correspond to quotation searches, currently just a random one
+var query_input = "cars"; // query input
+
+
+function getData(){
+	src="https://www.googleapis.com/customsearch/v1?key=AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY&cx=017576662512468239146:omuauf_lfve&q=cars&callback=hndlr";
+	console.log("getData executed")
+}
 
 
 function errorCode(){
@@ -13,11 +26,23 @@ function errorCode(){
 	alert('Error 1');
 }
 
-function getData(response) {
+function hndlr(response) {
 	//src = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY&cx=017576662512468239146:omuauf_lfve&q=cars&callback=hndlr";
 	for (var i = 0; i < response.items.length; i++) {
 		var item = response.items[i];
 		// in production code, item.htmlTitle should have the HTML entities escaped.
-		document.getElementById("test").innerHTML += "<br>" + item.htmlTitle;
+		document.getElementById("query").innerHTML += "<br>" + item.htmlTitle;
 	}
+	console.log("hndlr executed")
 }
+
+function triggerSearch(){
+  //var query = document.getElementById("query").value;
+	//var query = document.getElementById("query").innerHTML;
+  var JSElement = document.createElement('script');  // what does this do?
+  JSElement.src = "https://www.googleapis.com/customsearch/v1?key=" + api_key + "&cx=" + cse_id + "&q=" + query_input +"&callback=hndlr";
+  document.getElementsByTagName('head')[0].appendChild(JSElement);
+	console.log("triggersearch executed")
+}
+
+//triggerSearch();
