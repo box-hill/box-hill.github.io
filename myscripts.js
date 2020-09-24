be84117e6b2ac91f14bb91b5b7b5cf17dd622abb
box-hill <71170497+box-hill@users.noreply.github.com>
2020-09-24 17:27:31 +1000

    add files via upload

--- myscripts.js
+++ myscripts.js
@@ -11,7 +11,8 @@
 
 // google custom search api key:
 //var api_key = "AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY";
-var api_key = "AIzaSyCUMupOrK0nmg2uA8ez9XfA2_7aEEadAXg";
+//var api_key = "AIzaSyCUMupOrK0nmg2uA8ez9XfA2_7aEEadAXg";
+var api_key = "AIzaSyAezBsTrs0hsvsWZCCJy9Pgglb60weY7wM";
 
 // To create cse_id:
 // https://programmablesearchengine.google.com/about/
@@ -24,19 +25,22 @@ var cse_id = "22519e5637b61b1c8";
 //var query_input = "\"cars\""; // query input
 var query_input;
 
+// detect enter in submission form
+document.getElementById("search_input").addEventListener("keydown", function(event) {
+	if (event.keyCode === 13) {
+		console.log("enter detected")
+		event.preventDefault(); // Cancel the default action, if needed
+		document.getElementById("searchButton").click(); // Trigger the button element with a click
+	}
+});
 
-//doesnt work
+// just leaving for debugging
 function getData(){
-	src="https://www.googleapis.com/customsearch/v1?key=AIzaSyCUMupOrK0nmg2uA8ez9XfA2_7aEEadAXg&cx=22519e5637b61b1c8&q=\"学校を行かない\"&callback=hndlr";
+	src="https://www.googleapis.com/customsearch/v1?key=AIzaSyAezBsTrs0hsvsWZCCJy9Pgglb60weY7wM&cx=22519e5637b61b1c8&q=\"カメラから色を\"&callback=hndlr";
 	console.log("getData executed")
 }
 
-
-function errorCode(){
-// add error codes and switch case
-	alert('Error 1');
-}
-
+// search for exact string
 function stringHandler(response) {
 	//src = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBwOe5N5d275f_9-XYFfLMrcnm5xgqVJUY&cx=017576662512468239146:omuauf_lfve&q=cars&callback=hndlr";
 	// for (var i = 0; i < response.items.length; i++) {
@@ -45,29 +49,54 @@ function stringHandler(response) {
 	// 	// in production code, item.htmlTitle should have the HTML entities escaped.
 	// 	document.getElementById("query").innerHTML += "<br>" + item.htmlTitle;
 	// }
-	var item = response.queries;
-	document.getElementById("string_hits").innerHTML += "<br>" + item.request[0].totalResults;;
-	console.log(item.request[0].totalResults);
-	console.log("hndlr executed")
+
+	// check if search limit reached
+	if(response.hasOwnProperty('error')){
+		alert('Api key search limit reached.')
+		console.log('limit reached');
+		return;
+	}
+
+	document.getElementById("string_hits").innerHTML += "<br>" + response.queries.request[0].totalResults;
+	console.log('string hits = ' + response.queries.request[0].totalResults);
+
+	document.getElementById("example_sentences").innerHTML = ""; // erase previous
+	for (var i = 0; i < response.items.length; i++) {
+		var item = response.items[i];
+		console.log(item);
+		// in production code, item.htmlTitle should have the HTML entities escaped.
+		document.getElementById("example_sentences").innerHTML += "<br>" + item.htmlSnippet + "<br>";
+	}
+	//document.getElementById("example_sentences").innerHTML += "<br>" + response.queries.request[0].totalResults;
 }
 
+// general search for key words
 function generalHandler(response) {
-	var item = response.queries;
-	document.getElementById("general_hits").innerHTML += "<br>" + item.request[0].totalResults;;
-	console.log(item.request[0].totalResults);
-	console.log("hndlr executed")
+
+	if(response.hasOwnProperty('error')){
+		alert('Api key search limit reached.')
+		console.log('limit reached');
+		return;
+	}
+	document.getElementById("general_hits").innerHTML += "<br>" + response.queries.request[0].totalResults;
+	console.log(response.queries.request[0].totalResults);
+	console.log('general hits = ' + response.queries.request[0].totalResults);
 }
 
 
 function triggerSearch(){
   //var query = document.getElementById("query").value;
 	//var query = document.getElementById("query").innerHTML;
-
+	if(document.getElementById("search_input").value.length === 0){
+		alert('Input some text!'); // will make this not an alert msg later
+		return false; // stops function and returns
+	}
+	// check input for correctvalues
 	general_query = document.getElementById("search_input").value;
 	string_query = "\"" + document.getElementById("search_input").value + "\"";
 	//query_input =  document.getElementById("search_input").value;
 	//query_input = 'try';
-	console.log("query input is " + query_input);
+	console.log("query input is " + general_query);
 	console.log(document.getElementById("search_input"));
 
   var JSElement = document.createElement('script');  // what does this do?
